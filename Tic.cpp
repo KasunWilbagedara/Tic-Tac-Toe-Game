a11b0db771e8efa370aade66146560977807c432
Kasun Wilbagedara <kasunwilbagedara2001@gmail.com>
2025-04-10 23:10:15 +0530

    Complete Tic-Tac-Toe game implementation with all fixes

--- Tic.cpp
+++ Tic.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 char space[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 int row;
-int coloumn;
-char token='x';
-bool tie=false;
-string n1="";
-string n2="";
-void functionone(){
+int column;
+char token = 'x';
+bool isTie = false;
+string n1 = "";
+string n2 = "";
 
+void functionOne() {
     cout << "\n";
     cout << "   |       |   \n";
     cout << " " << space[0][0] << " |   " << space[0][1] << "   | " << space[0][2] << " \n";
@@ -22,170 +22,99 @@ void functionone(){
     cout << " " << space[2][0] << " |   " << space[2][1] << "   | " << space[2][2] << " \n";
     cout << "   |       |   \n";
 }
-  
-void functiontwo(){
-   int digit;
 
-   if(token=='x')
-   {
-      cout<<n1<<"please enter";
-      cin>>digit;
-   }
+void functionTwo() {
+    int digit;
+    
+    if(token == 'x') {
+        cout << n1 << " please enter: ";
+        cin >> digit;
+    }
+    else if(token == '0') {
+        cout << n2 << " please enter: ";
+        cin >> digit;
+    }
 
-   
-   if(token=='0')
-   {
-      cout<<n2<<"please enter";
-      cin>>digit;
-   }
+    if(digit == 1) { row = 0; column = 0; }
+    else if(digit == 2) { row = 0; column = 1; }
+    else if(digit == 3) { row = 0; column = 2; }
+    else if(digit == 4) { row = 1; column = 0; }
+    else if(digit == 5) { row = 1; column = 1; }
+    else if(digit == 6) { row = 1; column = 2; }
+    else if(digit == 7) { row = 2; column = 0; }
+    else if(digit == 8) { row = 2; column = 1; }
+    else if(digit == 9) { row = 2; column = 2; }
+    else {
+        cout << "Invalid input! Please enter a number between 1 and 9" << endl;
+        cin.clear();
+        cin.ignore(10000, '\n');
+        functionTwo();
+        return;
+    }
 
-   if(digit==1)
-   {
-      row=0;
-      column=0;
-
-   }
-
-    if(digit==2)
-   {
-      row=0;
-      column=1;
-      
-   }
-
-    if(digit==3)
-   {
-      row=0;
-      column=2;
-      
-   }
-
-    if(digit==4)
-   {
-      row=1;
-      column=0;
-      
-   }
-
-    if(digit==5)
-   {
-      row=1;
-      column=1;
-      
-   }
-
-    if(digit==6)
-   {
-      row=1;
-      column=2;
-      
-   }
-
-    if(digit==7)
-   {
-      row=2;
-      column=0;
-      
-   }
-
-
-   if(digit==8)
-   {
-      row=2;
-      column=1;
-      
-   }
-
-   if(digit==9)
-   {
-      row=2;
-      column=2 ;
-      
-   }
-
-   else if(digit<<1 || digit>9)
-   {
-      cout<<"invalid |||"<<endl;
-   }
-
-   if(token=='x'&& space [row][coloumn] !='x' && space[row][coloumn] !='0')
-{
-   space[row][coloumn]='x';
-   token='0';
+    if(space[row][column] != 'x' && space[row][column] != '0') {
+        space[row][column] = token;
+        token = (token == 'x') ? '0' : 'x';
+    }
+    else {
+        cout << "That space is already taken!" << endl;
+        functionTwo();
+    }
 }
 
-else if( token=='0'&& space [row][coloumn] !='x' && space[row][coloumn] !='0')
-{
-   space[row][coloumn]='0';
-   token='x';
+bool functionThree() {
+    for(int i = 0; i < 3; i++) {
+        if((space[i][0] == space[i][1] && space[i][1] == space[i][2]) ||
+           (space[0][i] == space[1][i] && space[1][i] == space[2][i]))
+            return true;
+    }
+
+    if((space[0][0] == space[1][1] && space[1][1] == space[2][2]) ||
+       (space[0][2] == space[1][1] && space[1][1] == space[2][0]))
+        return true;
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(space[i][j] != 'x' && space[i][j] != '0')
+                return false;
+        }
+    }
+    
+    isTie = true;
+    return false;
 }
 
-else{
-   cout<<"there is no empty space!"<<endl;
-   functiontwo();
-}
-functionone();
-}
-
-bool functionThree()
-{
-   for(int i=0;i<3;i++)
-   {
-      if(space[i][0]==space[i][1] && space[i][0]==space[i][2] || space[0][i]==space[1][i] && space[0][i]==space[2][i])
-      return true;
-   }
-
-   if(space[0][0]==space[1][1] && space[1][1]==space[2][2] || space[0][2]==space[1][1] && space[1][1]==space[2][0])
-   {
-   return true;
-   }
-   
-   for(int i=0;i<3;i++)
-   {
-      for(int j=0;j<3;j++)
-      {
-          if(space[i][j] != 'x' && space[i][j] != '0')
-            {
-               return false;
-            }
-      }
-   }
-
-   tie=true;
-   return false;
-
-}
-
-int main()
-{
-
-   cout << "Enter the name of the first player: \n";
+int main() {
+    cout << "Enter the name of the first player: ";
     getline(cin, n1);
-    cout << "Enter the name of the second player: \n";
+    cout << "Enter the name of the second player: ";
     getline(cin, n2);
-    cout << n1 << " is player1 so he/she will play first\n";
-    cout << n2 << " is player2 so he/she will play second\n";
-   while(!functionthree())
-   {
-      functionone();
-      functiontwo();
-      functionthree();
-   }
+    cout << n1 << " is player1 (x) so he/she will play first\n";
+    cout << n2 << " is player2 (0) so he/she will play second\n";
 
-   if(token=='x' && tie ==false )
-   {
-      cout<<n2<<"Wins!|"<<endl;
-   }
+    functionOne();
+    
+    while(!functionThree()) {
+        functionTwo();
+        functionOne();
+    }
 
-   else if(token=='0' && tie==false)
-   {
-      cout<<n1<<"Wins!|"<<endl;
-   }
-   else
-   {
-      cout<<"its a draw!"<<endl;
-   }
+    functionOne();
+    
+    if(isTie) {
+        cout << "\nGame Over! It's a draw!" << endl;
+    }
+    else if(token == 'x') {
+        cout << "\nGame Over! " << n2 << " (0) Wins!" << endl;
+    }
+    else {
+        cout << "\nGame Over! " << n1 << " (x) Wins!" << endl;
+    }
+
+    cout << "\nPress Enter to exit...";
+    cin.clear();             
+    cin.ignore(10000, '\n');  
+    cin.get();                
+
+    return 0;
 }
-
-
-
